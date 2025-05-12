b2047f414560e48d08a913c2882a78e716e24780
Zaky Irsyad Rais <zkyxentertain@gmail.com>
2025-05-12 18:33:41 +0700

    Refactor page function parameters in checkout and pay components to use Promise for id

--- src/app/pay/[id]/page.tsx
+++ src/app/pay/[id]/page.tsx
@@ -2,7 +2,11 @@ import Information from "@/components/layouts/pay/Information";
 import Transaction from "@/components/layouts/pay/Transaction";
 import React from "react";
 
-export default async function page({ params }: { params: { id: string } }) {
+export default async function page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
   return (
     <main className="grid grid-cols-1 md:grid-cols-2 max-w-7xl mx-auto">
